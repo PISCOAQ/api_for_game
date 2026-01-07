99c1072d89e13cbc2011788841a2954aee3add3d
Jacopo <jacopolorenzini0@gmail.com>
2026-01-07 08:19:14 +0100

    Aggiunti campi al model bambino

--- models/bambino.js
+++ models/bambino.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const BambinoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   cognome: { type: String, required: true },
+  dataNascita: {type: Date,required: true},
+  sesso: {type: String, enum: ['maschio', 'femmina'],required: true}
 
-  // puoi aggiungere nuovi campi in futuro, per ora teniamolo semplice
+
 }, {
-  timestamps: true
+  timestamps: true,
+  versionKey: false
 });
 
 module.exports = mongoose.model('Bambino', BambinoSchema);
