066f5aeb799dd610f3dda8dc97be47341ccec956
Jacopo <jacopolorenzini0@gmail.com>
2026-04-17 18:05:48 +0200

    aggiunta api per eliminare utenti e relativi test

--- routes/utente.routes.js
+++ routes/utente.routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   creaUtente,
@@ -7,30 +7,28 @@ const {
   getPercorsiAssegnati,
   updateProgressiGioco,
   getDatiUtentePerGioco,
-  updateCtxPercorso
-} = require('../controllers/utente.controller');
-const utente = require('../models/utente');
+  updateCtxPercorso,
+  deleteUtenti,
+} = require("../controllers/utente.controller");
+const utente = require("../models/utente");
 
 const {
   salvaDiagnosi,
   eliminaDiagnosi,
-} = require('../controllers/diagnosi.controller');
+} = require("../controllers/diagnosi.controller");
 
+router.post("/utente", creaUtente);
+router.get("/utente", listaUtenti);
+router.post("/utenti/delete", deleteUtenti);
 
+router.post("/utenti/:id/assegna-percorso", assegnaPercorso);
+router.get("/utenti/:codiceGioco/percorsi", getPercorsiAssegnati);
 
-
-router.post('/utente', creaUtente);
-router.get('/utente', listaUtenti);
-
-router.post('/utenti/:id/assegna-percorso', assegnaPercorso);
-router.get('/utenti/:codiceGioco/percorsi', getPercorsiAssegnati);
-
-router.patch('/utenti/:codiceGioco/progressi', updateProgressiGioco);
-router.get('/utente/:codiceGioco', getDatiUtentePerGioco);
+router.patch("/utenti/:codiceGioco/progressi", updateProgressiGioco);
+router.get("/utente/:codiceGioco", getDatiUtentePerGioco);
 router.patch("/utenti/:codiceGioco/ctx", updateCtxPercorso);
 
-router.put('/utenti/:id/diagnosi', salvaDiagnosi);
-router.delete('/utenti/:id/diagnosi', eliminaDiagnosi);
-
+router.put("/utenti/:id/diagnosi", salvaDiagnosi);
+router.delete("/utenti/:id/diagnosi", eliminaDiagnosi);
 
 module.exports = router;
